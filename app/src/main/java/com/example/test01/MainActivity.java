4eb91eedf70511cf049fc244dcfff991b5256d46
av4195 <av4195@bs.amalnet.k12.il>
2024-12-02 12:58:00 +0200

    סהסה add todo

--- app/src/main/java/com/example/test01/MainActivity.java
+++ app/src/main/java/com/example/test01/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //TODO: finish the ?
     }
 }
